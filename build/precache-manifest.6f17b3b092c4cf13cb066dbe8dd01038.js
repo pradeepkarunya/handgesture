006116dd5c2603f00905d1e17093043f469cecc8
pradeepkarunya <pradeepkarunya@gmail.com>
2021-03-08 19:41:16 +0530

    Add files via upload

---
+++ build/precache-manifest.6f17b3b092c4cf13cb066dbe8dd01038.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7b1d318ba4a3860c5c28f27f2d651cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d413716a589fce54343",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "67389536c39c36e06072",
+    "url": "/static/js/2.db68ecf8.chunk.js"
+  },
+  {
+    "revision": "93ce851949c81c033c28f6758a3c33f3",
+    "url": "/static/js/2.db68ecf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d413716a589fce54343",
+    "url": "/static/js/main.4c43802f.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  },
+  {
+    "revision": "7c27ffed1c9fd611fc0a973bd2433e24",
+    "url": "/static/media/i_love_you.7c27ffed.png"
+  },
+  {
+    "revision": "1cfa24a5e82c665b700f38d9bdf04195",
+    "url": "/static/media/thumbs_up.1cfa24a5.png"
+  },
+  {
+    "revision": "6db44b04476f1385864179e398b16fa0",
+    "url": "/static/media/victory.6db44b04.png"
+  }
+]);
